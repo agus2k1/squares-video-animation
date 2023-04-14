e8a5f02f543df146bcdcf607599fc59c229433a4
West <agus2kcoin@outlook.com>
2023-04-13 23:18:43 -0300

    Add plane vertex shader

---
+++ shaders/vertex.glsl.js
@@ -0,0 +1,13 @@
+const vertexShader = /* glsl */ `
+  varying vec2 vUv;
+
+  void main() {
+    vUv = uv;
+
+    vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
+    gl_PointSize = 10. * ( 1. / - mvPosition.z );
+    gl_Position = projectionMatrix * mvPosition;
+  }
+`;
+
+export default vertexShader;
